647ed5a282e836848b5917f94c37e98b7c61274b
ANTON ORTEGA RIVERA <antonortega17@gmail.com>
2020-07-06 17:03:36 -0500

    CONEXION DB
    - se conecta a la base de datos creada
    - llena la info correspondiente a cada "lote" y lo muestra mediante click ; al igual que pinta la disponibilidad de cada lote

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,7 +4,34 @@ import HomePage from '../pages/home.f7.html';
 var routes = [
   {
     path: '/',
-    component: HomePage,
+
+    async: function (routeTo, routeFrom, resolve, reject) {
+      // Router instance
+      var router = this;
+      // App instance
+      var app = router.app;
+      var campanaId = routeTo.params.campanaId;
+      // Show Preloader
+      app.preloader.show();
+      app.request.promise.json('https://192.168.5.124/puntabosque/items/lotes')
+      .then(function (res) {
+        console.log(res);
+        app.preloader.hide();
+        resolve(
+          {
+            component: HomePage,
+          },
+          {
+            context: {
+              //server: app.data.server,
+              //campana: res.data.data,
+              lotes: res.data,
+            }
+          }
+        );
+      });
+    }
+
   },
 ];
 
